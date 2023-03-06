7f357a825043258835ece2f35b1579d17c10de66
Timothé <timothegauquelin@gmail.com>
2023-03-06 22:51:18 +0100

    [REFACTO][TG] - Object Name refacto

--- src/test/java/com/example/trial/utils/ApologyTestUtils.java
+++ src/test/java/com/example/trial/utils/ApologyTestUtils.java
@@ -3,9 +3,6 @@ package com.example.trial.utils;
 import com.example.trial.model.Apology;
 import com.example.trial.model.ApologyTag;
 
-import java.time.LocalDate;
-import java.util.Set;
-
 public class ApologyTestUtils {
 
     public static Apology apologyTest(String message, String apologyTagLabel
@@ -14,12 +11,12 @@ public class ApologyTestUtils {
                 .label(apologyTagLabel)
                 .build();
 
-        Apology archetype = Apology.builder()
+        Apology apology = Apology.builder()
                 .message(message)
                 .apologyTag(apologyTag)
                 .build();
 
-        return archetype;
+        return apology;
     }
 
 }
